f8eaf2454bb336ca6e55e0c554cc6064f0f8a0a9
cnridley <cnridley1@gmail.com>
2021-03-10 04:04:20 +0000

    made menu page and made it moibile responsive

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Menu(models.Model):
+    pie_name = models.CharField(max_length=254, null=True, blank=True)
+    pie_description = models.CharField(max_length=254, null=True, blank=True)
+    pie_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.pie_name
--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import Menu
 
 # Create your views here.
 
 def menu(request):
     """A view to return menu page"""
+    pie = Menu.objects.all()
+    context = {
+        'pie': pie,
+    }
 
-    return render(request, 'menu.html')
+    return render(request, 'menu.html', context)
