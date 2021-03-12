1e85d4ba3f908cae3ddc6156bcc02c22ef77fbba
cnridley <cnridley1@gmail.com>
2021-03-12 02:58:14 +0000

    set up heroku and installed required packages:

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pieandsell.wsgi:application
--- pieandsell/settings.py
+++ pieandsell/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p2r%j%a^kzcw_ve*g7u+y$ffx5jlknddu3rp9+ka0sa%g15glk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['canmore-pie-and-sell.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'pieandsell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    } 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
