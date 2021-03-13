9110d412be7d6713528950f133f1a7e515e87e5c
cnridley <cnridley1@gmail.com>
2021-03-13 16:41:33 +0000

    removed secret key

--- pieandsell/settings.py
+++ pieandsell/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '') 
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-#SECRET_KEY = '6&ra0w3$x9_$66j8_0#pq9xa4efek&$dpn6u_jnq00**1mt3p7' #DELETE
+
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = 'DEVELOPMENT' in os.environ
-#DEBUG = True #DELETE
+
 ALLOWED_HOSTS = ['canmore-pie-and-sell.herokuapp.com', 'localhost']
 
 
