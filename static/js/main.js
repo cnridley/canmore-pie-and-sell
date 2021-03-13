928c312838a3b7b3b6a802b64fb4aaedb728624e
cnridley <cnridley1@gmail.com>
2021-03-13 16:14:23 +0000

    added js for click here to order

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,11 @@
-//$(document).ready(function(){
-//    $('.soldOut').hide();
-   // Check or Uncheck All checkboxes
- //  $(".Sold").click(function(){
-   //     if ($(this).prop("checked") == true){
-  //      $('.soldOut').show();
-    //} else if ($(this).prop("checked") == false) {
-      //  $('.soldOut').hide();
-    //}
-    //});
-//});
+$(document).ready(function(){
+    $('#Sold').hide();
+    //Check or Uncheck All checkboxes
+   $(".show").click(function(){
+       if ($(this).prop("checked") == true){
+      $('#Sold').show();
+    } else if ($(this).prop("checked") == false) {
+        $('#Sold').hide();
+    }
+    });
+});
